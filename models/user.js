fce5bf4a5c599bd3f8fe5d7095cf00c1aae02fc5
Mr Rexac <rexac@Mrs-MacBook-Pro.local>
2018-08-24 13:11:54 -0400

    Mac

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UserSchema = mongoose.Schema({
+email: {
+  type: String,
+  required: true,
+  trim: true,
+  minlength: 9,
+  unique: true,
+  validate: {
+    validator: validator.isEmail,
+    message: '{VALUE} IS NOT A VALID EMAIL'
+  }
+},
+password: {
+  type: String,
+  required: true,
+  minlength: 6,
+},
+tokens: [{
+  access: {
+    type: String,
+    required: true
+  },
+  token :{
+    type: String,
+    required: true
+  }
+}]
+});
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  })
+};
+
+var User = mongoose.model('Users', UserSchema);
+
+module.exports = {User};
